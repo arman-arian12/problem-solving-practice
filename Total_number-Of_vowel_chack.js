6a07473b150e94c816978fe956f1efa94d0172a9
Md Arman <106583530+arman-arian12@users.noreply.github.com>
2022-11-03 16:57:06 +0600

    Total number Of vowel chack

---
+++ Total_number-Of_vowel_chack.js
@@ -0,0 +1,17 @@
+//কোন sentence এ কতগুলো vowel আছে তা নির্ণয় করা যায় কিভাবে?
+
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+function countvowiel(sentence) {
+	let count = 0;
+	const letter = Array.from(sentence);
+
+	letter.forEach(function(value) {
+		if (vowels.includes(value)) {
+			count++;
+		}
+	});
+	return count;
+}
+
+console.log(countvowiel("I LOVE BANGLADESHh"));
