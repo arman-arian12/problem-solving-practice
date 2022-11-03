5d9b53c91ac1fc84dbab562327f6ddab3314ca18
Md Arman <106583530+arman-arian12@users.noreply.github.com>
2022-11-03 16:40:27 +0600

    Add files via upload
    Vowel and Consonant chacking program build by js.

---
+++ Js-problem-solving/index.js
@@ -0,0 +1,31 @@
+//    vowel or consonenrt chack
+var latter=prompt("Enter your latter:");
+
+latter=latter.toLowerCase();
+switch(latter){
+
+    case"a":
+    console.log("vowel");
+    break;
+
+
+    case"e":
+    console.log("vowel");
+    break;
+    case"i":
+    console.log("vowel");
+    break;
+    case"o":
+    console.log("vowel");
+    break;
+    case"u":
+    console.log("vowel");
+    break;
+
+    default:
+        console.log("consolent");;
+    
+    
+}
+
+
